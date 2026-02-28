b8e5e9373fa51129e9a98ed19eca40be1538144d
CleaningSoap <bruce.otw@gmail.com>
2026-02-28 16:20:49 -0500

    Update player.gd

--- campfire/Scripts/player.gd
+++ campfire/Scripts/player.gd
@@ -1,3 +1,14 @@
 extends CharacterBody2D
 
 class_name Player
+
+signal take_damage_signal(damage:int)
+
+func _ready() -> void:
+	take_damage_signal.connect(take_damage)
+	
+func take_damage(damage:int):
+	pass
+	
+func _physics_process(delta: float) -> void:
+	pass
